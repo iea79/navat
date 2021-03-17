e4f9a2f29833eb9fbf696976edafd104edbfe5c6
Evgeny Ivanov <busforward@gmail.com>
2021-03-17 12:28:43 +0500

    добавил анимации и прелоадер при загрузке страницы

--- js/function.js
+++ js/function.js
@@ -5,10 +5,10 @@
  * Skype: ivanov_ea
  *
  */
-
-
 const nav = document.querySelector('.nav');
 const navToggle = document.querySelectorAll('.nav__toggle');
+const img = document.querySelectorAll('.img');
+const loader = document.querySelector('.loader');
 
 navToggle.forEach((item) => {
     item.addEventListener('click', () => {
@@ -25,3 +25,27 @@ function hideMenu(e) {
         nav.classList.remove('open');
     }
 }
+
+// Скрываем блоки с картинками, готовим анимацию появления
+img.forEach(item => {
+    const parent = item.parentNode;
+    parent.style.opacity = 0;
+    parent.style.transform = 'translateY(20px)';
+});
+
+// После загрузки страницы
+window.addEventListener('load', () => {
+    // Прячем лоадер
+    loader.classList.add('hide');
+
+    // Показываем блоки с картинками поочередно c задержкой в 200мс после загрузки страницы
+    setTimeout(() => {
+        img.forEach((item, i) => {
+            const parent = item.parentNode;
+            setTimeout(() => {
+                parent.style.opacity = 1;
+                parent.style.transform = 'translateY(0px)';
+            }, 100 * i);
+        });
+    }, 200);
+});
