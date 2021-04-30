ba6c7e80b8c5f3e18d75960353fe8fc6edc35451
Egeny Ivanov <busforward@gmail.com>
2021-04-30 16:55:15 +0500

    add lightbox

--- js/function.js
+++ js/function.js
@@ -8,6 +8,8 @@
 const nav = document.querySelector('.nav');
 const navToggle = document.querySelectorAll('.nav__toggle');
 const img = document.querySelectorAll('.img');
+const lightboxTrigger = document.querySelectorAll('[data-lightbox]');
+const lightboxClose = document.querySelector('.lightbox__close');
 const loader = document.querySelector('.loader');
 
 navToggle.forEach((item) => {
@@ -26,26 +28,55 @@ function hideMenu(e) {
     }
 }
 
-// Скрываем блоки с картинками, готовим анимацию появления
-img.forEach(item => {
-    const parent = item.parentNode;
-    parent.style.opacity = 0;
-    parent.style.transform = 'translateY(20px)';
+
+
+lightboxTrigger.forEach(item => {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        let lightbox = document.querySelector('.lightbox'),
+            lightboxContent = lightbox.querySelector('.lightbox__content'),
+            src = this.href;
+
+        lightboxContent.innerHTML = `<img src=${src} />`;
+        lightbox.classList.add('open');
+        document.body.classList.add('not_scroll');
+
+    });
 });
+
+lightboxClose.addEventListener('click', () => {
+    let lightbox = document.querySelector('.lightbox'),
+        lightboxContent = lightbox.querySelector('.lightbox__content');
+
+    lightboxContent.innerHTML = ``;
+    lightbox.classList.remove('open');
+    document.body.classList.remove('not_scroll');
+});
+
+// Скрываем блоки с картинками, готовим анимацию появления
+// img.forEach(item => {
+//     const parent = item.parentNode;
+//     parent.style.opacity = 0;
+//     parent.style.transform = 'translateY(20px)';
+// });
 
 // После загрузки страницы
 window.addEventListener('load', () => {
     // Прячем лоадер
-    loader.classList.add('hide');
+    try {
+        loader.classList.add('hide');
+    } finally {
+
+    }
 
     // Показываем блоки с картинками поочередно c задержкой в 200мс после загрузки страницы
-    setTimeout(() => {
-        img.forEach((item, i) => {
-            const parent = item.parentNode;
-            setTimeout(() => {
-                parent.style.opacity = 1;
-                parent.style.transform = 'translateY(0px)';
-            }, 100 * i);
-        });
-    }, 200);
+    // setTimeout(() => {
+    //     img.forEach((item, i) => {
+    //         const parent = item.parentNode;
+    //         setTimeout(() => {
+    //             parent.style.opacity = 1;
+    //             parent.style.transform = 'translateY(0px)';
+    //         }, 100 * i);
+    //     });
+    // }, 200);
 });
